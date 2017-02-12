bfc62e73840b391a9f7d1502371203ee2c7dcb52
Stefan Pel <Stefan@MacBook-Pro-van-Stefan-2.local>
2017-02-12 15:33:03 +0100

    Initialized switches

--- Mr. Potatohead/ViewController.swift
+++ Mr. Potatohead/ViewController.swift
@@ -10,16 +10,106 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // outlets for images
+    @IBOutlet weak var hat: UIImageView!
+    @IBOutlet weak var eyes: UIImageView!
+    @IBOutlet weak var glasses: UIImageView!
+    @IBOutlet weak var shoes: UIImageView!
+    @IBOutlet weak var ears: UIImageView!
+    @IBOutlet weak var mouth: UIImageView!
+    @IBOutlet weak var mustache: UIImageView!
+    @IBOutlet weak var eyebrows: UIImageView!
+    @IBOutlet weak var nose: UIImageView!
+    @IBOutlet weak var arms: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // switches
+    
+    @IBAction func HatSwitch(_ sender: UISwitch) {
+        if hat.isHidden == true {
+            hat.isHidden = false
+        }
+        else {
+            hat.isHidden = true
+        }
+    }
+    @IBAction func EyesSwitch(_ sender: UISwitch) {
+        if eyes.isHidden == true {
+            eyes.isHidden = false
+        }
+        else {
+            eyes.isHidden = true
+        }
+    }
+    @IBAction func GlassesSwitch(_ sender: UISwitch) {
+        if glasses.isHidden == true {
+            glasses.isHidden = false
+        }
+        else {
+            glasses.isHidden = true
+        }
+    }
+    @IBAction func ShoesSwitch(_ sender: UISwitch) {
+        if shoes.isHidden == true {
+            shoes.isHidden = false
+        }
+        else {
+            shoes.isHidden = true
+        }
+    }
+    @IBAction func EarsSwitch(_ sender: UISwitch) {
+        if ears.isHidden == true {
+            ears.isHidden = false
+        }
+        else {
+            ears.isHidden = true
+        }
+    }
+    @IBAction func MouthSwitch(_ sender: UISwitch) {
+        if mouth.isHidden == true {
+            mouth.isHidden = false
+        }
+        else {
+            mouth.isHidden = true
+        }
+    }
+    @IBAction func MustacheSwitch(_ sender: UISwitch) {
+        if mustache.isHidden == true {
+            mustache.isHidden = false
+        }
+        else {
+            mustache.isHidden = true
+        }
+    }
+    @IBAction func EyebrowsSwitch(_ sender: UISwitch) {
+        if eyebrows.isHidden == true {
+            eyebrows.isHidden = false
+        }
+        else {
+            eyebrows.isHidden = true
+        }
+    }
+    @IBAction func NoseSwitch(_ sender: UISwitch) {
+        if nose.isHidden == true {
+            nose.isHidden = false
+        }
+        else {
+            nose.isHidden = true
+        }
+    }
+    @IBAction func ArmsSwitch(_ sender: UISwitch) {
+        if arms.isHidden == true {
+            arms.isHidden = false
+        }
+        else {
+            arms.isHidden = true
+        }
+    }
 }
-
